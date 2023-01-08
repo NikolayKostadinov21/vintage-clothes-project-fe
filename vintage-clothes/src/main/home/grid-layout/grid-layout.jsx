b44643697d452a77a8b363b2be8eaccdcf2d3706
Nikolay Kostadinov <nikolay.kostadinov@pliant.io>
2023-01-08 15:05:55 +0200

    VCP-1 Creating the grid layout in the home page

---
+++ vintage-clothes/src/main/home/grid-layout/grid-layout.jsx
@@ -0,0 +1,286 @@
+import '../grid-layout/grid-layout.scss' ;
+
+const GridLayout = () => {
+    return (
+        <>
+            <div className='margin-top'>
+            </div>
+            <div className='home-page-items-container'>
+                <div className='items-container'>
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            <div className='publisher-name'>
+                                Nikolayski87
+                            </div>
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            <div className='publisher-name'>
+                                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                            </div>
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='item'>
+                        <div className='item-details-publisher'>
+                            <img className='publisher-picture' src='https://via.placeholder.com/300x400' />
+                            Nikolayski87
+                        </div>
+                        <div className='item-image'>
+                            <img src='https://via.placeholder.com/300x400' />
+                        </div>
+                        <div className='item-details'>
+                            {/* <div className='item-details-publisher'>
+                                Nikolayski87
+                                <img src='https://via.placeholder.com/300x400' />
+                            </div> */}
+                            <div className='item-details-price-size-description'>
+                                <div className='item-details-price'>
+                                    12$
+                                </div>
+                                <div className='item-details-size'>
+                                    M
+                                </div>
+                                <div className='item-details-description'>
+                                    T-shirt
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default GridLayout;
